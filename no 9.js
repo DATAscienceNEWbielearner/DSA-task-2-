7fe1dcdebd4e93acf43da218d91b04283d952409
DATAscienceNEWbielearner <umangyadav1310datascience@gmail.com>
2025-12-14 14:45:44 +0530

    Create no 9.js

---
+++ no 9.js
@@ -0,0 +1,8 @@
+let numbers = [ 1,3,4,5,6,7,8,9,10];
+let totalsum = (10/2)*(10+1);
+let sum = 0;
+for( num of numbers) {
+sum +=num;
+}
+console.log(( totalSum-sum));
+// this ans is write in js code unlike others if u are watching this its my office laptop there is no python in this i am commiting my changes!!
